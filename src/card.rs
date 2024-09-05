727f1e335197b4f9b84538203cfab375a06e94a8
Miguel D. Salcedo <miguel@salcedo.cc>
2024-09-04 23:42:39 -0400

    Implement a method to wait until a card is present and update read to capture the lack of a card in its type.

--- src/card.rs
+++ src/card.rs
@@ -1,6 +1,7 @@
 use anyhow::anyhow;
-use pcsc::Context;
+use pcsc::{Card, Context, ReaderState, State};
 use std::ffi::CString;
+use std::time::Duration;
 
 pub struct Reader {
     ctx: Context,
@@ -12,35 +13,51 @@ impl Reader {
         Reader { ctx, reader }
     }
 
-    pub fn read(&self) -> anyhow::Result<String> {
-        let card = match self.ctx.connect(&self.reader, pcsc::ShareMode::Direct, pcsc::Protocols::UNDEFINED) {
-            Ok(card) => card,
+    pub fn read(&self) -> anyhow::Result<Option<String>> {
+        match self.connect()? {
+            None => Ok(None),
+            Some(card) => {
+                // Send an APDU command.
+                let apdu = b"\x00\xa4\x04\x00\x0A\xA0\x00\x00\x00\x62\x03\x01\x0C\x06\x01";
+                println!("Sending APDU: {:?}", apdu);
+                let mut rapdu_buf = [0; 1024];
+                let rapdu = match card.transmit(apdu, &mut rapdu_buf) {
+                    Ok(rapdu) => rapdu,
+                    Err(err) => {
+                        eprintln!("Failed to transmit APDU command to card: {}", err);
+                        std::process::exit(1);
+                    }
+                };
+                println!("APDU response: {:?}", rapdu);
+                Ok(Some(String::new()))
+            }
+        }
+    }
+
+    fn connect(&self) -> anyhow::Result<Option<Card>> {
+        match self.ctx.connect(&self.reader, pcsc::ShareMode::Direct, pcsc::Protocols::UNDEFINED) {
+            Ok(card) => Ok(Some(card)),
             Err(pcsc::Error::NoSmartcard) => {
-                return Err(anyhow!("A smartcard is not present in the reader."))?;
+                Ok(None)
             }
             Err(err) => {
-                return Err(anyhow!(err))?;
+                Err(anyhow!(err))
             }
-        };
-
-        // Send an APDU command.
-        let apdu = b"\x00\xa4\x04\x00\x0A\xA0\x00\x00\x00\x62\x03\x01\x0C\x06\x01";
-        println!("Sending APDU: {:?}", apdu);
-        let mut rapdu_buf = [0; 1024];
-        let rapdu = match card.transmit(apdu, &mut rapdu_buf) {
-            Ok(rapdu) => rapdu,
-            Err(err) => {
-                eprintln!("Failed to transmit APDU command to card: {}", err);
-                std::process::exit(1);
-            }
-        };
-        println!("APDU response: {:?}", rapdu);
-
-        Ok(String::new())
+        }
     }
 
     pub fn write(&self, value: String) -> anyhow::Result<()> {
         let card = self.ctx.connect(&self.reader, pcsc::ShareMode::Direct, pcsc::Protocols::UNDEFINED)?;
+
+        Ok(())
+    }
+
+    pub fn wait(&self, timeout: Option<Duration>) -> anyhow::Result<()> {
+        let mut reader_states = [ReaderState::new(self.reader.clone(), State::UNAWARE)];
+
+        while !reader_states[0].event_state().contains(State::PRESENT) {
+            self.ctx.get_status_change(timeout, &mut reader_states)?;
+        }
 
         Ok(())
     }
