e7a0a61732dec63df2d64426212ee2887720b7c0
Miguel D. Salcedo <miguel@salcedo.cc>
2024-09-11 21:44:28 -0400

    Allow tagger to run without a reader.

--- src/bin/tagger.rs
+++ src/bin/tagger.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use slint::SharedString;
 
 slint::include_modules!();
@@ -7,36 +8,46 @@ fn main() -> Result<(), slint::PlatformError> {
 
     ui.on_request_read_value({
         let ui_handle = ui.as_weak();
-        let read_ctx =
-            pcsc::Context::establish(pcsc::Scope::User).expect("Failed to establish context");
-        let reader = jukebox::choose_reader(read_ctx).expect("Failed to choose a card reader.");
 
         move || {
             if let Some(ui) = ui_handle.upgrade() {
-                if let Some(value) = reader
-                    .read()
-                    .expect("Failed to read the URI from the card.")
-                {
-                    ui.set_value(SharedString::from(value));
-                }
+                let value = read_value().expect("Failed to read the URI from the card.");
+                ui.set_value(SharedString::from(value));
             }
         }
     });
 
     ui.on_request_write_value({
         let ui_handle = ui.as_weak();
-        let write_ctx =
-            pcsc::Context::establish(pcsc::Scope::User).expect("Failed to establish context");
-        let reader = jukebox::choose_reader(write_ctx).expect("Failed to choose a card reader.");
 
         move || {
             if let Some(ui) = ui_handle.upgrade() {
-                reader
-                    .write(ui.get_value().as_str().to_string())
+                write_value(ui.get_value().as_str().to_string())
                     .expect("Failed to write the URI to the card.");
             }
         }
     });
 
     ui.run()
+}
+
+fn read_value() -> anyhow::Result<String> {
+    let ctx = pcsc::Context::establish(pcsc::Scope::User)?;
+    let reader = jukebox::choose_reader(ctx)?;
+
+    match reader.read()? {
+        None => Err(anyhow!("No card is present.")),
+        Some(value) => Ok(value)
+    }
+}
+
+fn write_value(value: String) -> anyhow::Result<()> {
+    let ctx = pcsc::Context::establish(pcsc::Scope::User)?;
+    let reader = jukebox::choose_reader(ctx)?;
+
+    if reader.write(value)? {
+        return Err(anyhow!("No card is present."));
+    } else {
+        Ok(())
+    }
 }
