f7f4645b6743061e4989013b0fbc5ed2b2c72371
Miguel D. Salcedo <miguel@salcedo.cc>
2024-10-13 09:47:44 -0400

    Add logging on skip

--- src/player.rs
+++ src/player.rs
@@ -151,6 +151,7 @@ fn start_playback(
     let mut iterator = request.uris.iter().rev();
     if iterator.next() != current.as_ref() && iterator.any(|i| Some(i) == current.as_ref()) {
         client.skip_to_next(None)?;
+        tracing::info!(%?current, %request.uris, "Skipping to the next song in the queue");
         return Ok(playable);
     }
 
